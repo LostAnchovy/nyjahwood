149722e073d67b0704a562779e882013696fb020
Keith Nguyen <khoacn@gmail.com>
2018-10-03 09:39:42 -0700

    added navbar authentication

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+isAuthenticated:boolean = false;
+username= "keith"
+  
+constructor() { }
 
   ngOnInit() {
   }
